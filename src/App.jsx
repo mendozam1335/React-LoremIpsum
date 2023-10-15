8b1fdce86f3027a77470535ed2177719fa5de5bf
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-10-15 12:06:59 -0500

    Created form, input and button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,23 @@
+import { useState } from "react";
+import text from "./data";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [paragraph, setParagraph] = useState(text);
+
+  return (
+    <div className="section-center">
+      <form className="form">
+        <h4 className="title">Tired of Boring old Lorem Ipsum</h4>
+        <div className="lorem-form">
+          <label htmlFor="amount">Paragraphs:</label>
+          <input type="number" name="amount" id="amount" value={count} />
+          <button type="submit" className="btn">
+            Generate
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 export default App;
