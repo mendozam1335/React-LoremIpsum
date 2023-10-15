fb192b5fb365521c70f4cb9e5b054cf54c651525
mendozam1335 <55033050+mendozam1335@users.noreply.github.com>
2023-10-15 12:17:11 -0500

    Added functionality to generate paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,41 @@ import text from "./data";
 
 const App = () => {
   const [count, setCount] = useState(1);
-  const [paragraph, setParagraph] = useState(text);
-
+  const [paragraphs, setParagraphs] = useState([]);
+  const handleChange = (e) => {
+    setCount(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newParagraph = text.slice(0, count);
+    console.log(newParagraph);
+    setParagraphs(newParagraph);
+  };
   return (
     <div className="section-center">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <h4 className="title">Tired of Boring old Lorem Ipsum</h4>
         <div className="lorem-form">
           <label htmlFor="amount">Paragraphs:</label>
-          <input type="number" name="amount" id="amount" value={count} />
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            value={count}
+            min={1}
+            max={8}
+            step={1}
+            onChange={handleChange}
+          />
           <button type="submit" className="btn">
             Generate
           </button>
+        </div>
+        <div className="lorem-text">
+          {paragraphs &&
+            paragraphs.map((paragraph) => {
+              return <p>{paragraph}</p>;
+            })}
         </div>
       </form>
     </div>
